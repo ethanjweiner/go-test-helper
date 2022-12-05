b64d6b4d632f8aaeeec4d83173369ad2261cace2
Ethan Weiner <ethanweiner20@gmail.com>
2022-12-05 11:52:57 -0500

    feat: test on individual test basis

--- testhelper/testhelper.go
+++ testhelper/testhelper.go
@@ -10,10 +10,11 @@ type TestCase[Input any] struct {
 	Name     string
 	Args     Input
 	Expected any
+	Test     func(TestCase[Input])
 }
 
-func Run_Tests[Input any](t *testing.T, tests []TestCase[Input], testFunc func(TestCase[Input])) {
+func Run_Tests[Input any](t *testing.T, tests []TestCase[Input], defaultTest func(TestCase[Input])) {
 	for _, test := range tests {
-		t.Run(test.Name, func(*testing.T) { testFunc(test) })
+		t.Run(test.Name, func(*testing.T) { defaultTest(test) })
 	}
 }
